a2814685a336625b340cd6cebbf6c12d1758a5cb
Анна Шмакова <118816204+meesgloot@users.noreply.github.com>
2023-09-22 10:15:57 +0300

    Update script11.js

--- js/script11.js
+++ js/script11.js
@@ -8,7 +8,7 @@ const Block = {
     methods:{
         blo(){
             if(this.color == 'pink'){
-                this.title = 'Мой любимый принцип Барбары Лисков. А вообще я топлю за KISS(«Keep it simple, stupid»)'
+                this.title = 'Мой любимый принцип Барбары Лисков. А вообще, я топлю за KISS(«Keep it simple, stupid»)'
                 this.color = "aqua"
             }else{
                 this.title = 'Принципы SOLID'
